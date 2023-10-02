ea226ec58c711f79e7ad7872222af95b9d8f2065
Vijay Murari Tiyyala <vijaymurari.tiyyala@gmail.com>
2023-10-01 21:54:42 -0400

    Update peftlambda.sh

--- training/peftlambda.sh
+++ training/peftlambda.sh
@@ -9,8 +9,8 @@ conda activate llmtrain
 
 
 # Set W&B and HuggingFace API keys
-export WANDB_API_KEY='777501c1a468cab3359a9d2ee89293c06605a76e'
-export HUGGINGFACE_TOKEN='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'
+export WANDB_API_KEY='my key'
+export HUGGINGFACE_TOKEN='my key'
 
 # Running the training script
 python train.py \
