153b33e93763f673ab0adee8b222dce1f939e9e8
Vijay Murari Tiyyala <vijaymurari.tiyyala@gmail.com>
2023-11-21 16:43:19 -0500

    Update run_deep.sh

--- training/run_deep.sh
+++ training/run_deep.sh
@@ -43,8 +43,8 @@ pip install -r requirements.txt
 
 # Set your Weights & Biases API key
 echo "Setting W&B API key..."
-export WANDB_API_KEY='777501c1a468cab3359a9d2ee89293c06605a76e'
-export HUGGINGFACE_TOKEN='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'
+export WANDB_API_KEY='key'
+export HUGGINGFACE_TOKEN='key'
 
 # runs your code
 echo "Running python script..."
