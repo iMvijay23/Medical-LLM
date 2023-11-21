6d6a564caa0e9a9159084c042e659be70c427118
Vijay Murari Tiyyala <vijaymurari.tiyyala@gmail.com>
2023-11-21 16:43:44 -0500

    Update run_deepspeed.sh

--- training/run_deepspeed.sh
+++ training/run_deepspeed.sh
@@ -28,8 +28,8 @@ pip install nvidia-ml-py3
 
 # Set your Weights & Biases API key
 echo "Setting W&B API key..."
-export WANDB_API_KEY='777501c1a468cab3359a9d2ee89293c06605a76e'
-export HUGGINGFACE_TOKEN='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'
+export WANDB_API_KEY='key'
+export HUGGINGFACE_TOKEN='key'
 accelerate config
 accelerate env
 # runs your code
